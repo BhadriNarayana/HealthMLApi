117dbde770b119ccf4728683f358876e39bf05cf
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-05-08 15:46:20 +0530

    defining api function

--- api/views.py
+++ api/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 import tensorflow as tf
-# Create your views here.
+from rest_framework.decorators import api_view
 from django.templatetags.static import static
+import cv2
 
+model  = tf.keras.models.load_model('static/brain_tumor.h5')
 def home(request):
-    url = static('api/brain_tumor.h5')
-    model  = tf.keras.models.load_model(url)
+    img = cv2.imread('static/test_glioma.jpg')
+    
+    img = tf.image.resize(img, (256, 256))
 
+    
+    res = model.predict(tf.expand_dims(img, 0))
+    
 
-    return render(request, 'api/home.html', {})
+    return render(request, 'api/home.html', {'res': res})
 

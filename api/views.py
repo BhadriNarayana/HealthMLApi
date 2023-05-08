b43e44e75f5bb034106c58916d8b4fb796ca422d
BhadriNarayana <bhadrinarayana445@gmail.com>
2023-05-08 14:30:02 +0530

    static files correction

--- api/views.py
+++ api/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-
+import tensorflow as tf
 # Create your views here.
-
+from django.templatetags.static import static
 
 def home(request):
-    
+    url = static('api/brain_tumor.h5')
+    model  = tf.keras.models.load_model(url)
+
+
     return render(request, 'api/home.html', {})
 
